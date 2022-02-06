45fce8f0c3432825346b2b2b1569684a45009578
takashi247 <takashi247@gmail.com>
2022-02-06 14:35:54 +0900

    add files for ex01

---
+++ ex01/include/iter.hpp
@@ -0,0 +1,24 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <cstddef>
+#include <iostream>
+
+template <typename T>
+void printValue(T& value) {
+  std::cout << value << " ";
+}
+
+template <typename T>
+void iter(T* array, std::size_t len, void(*func)(T&)) {
+  for (std::size_t i = 0; i < len; ++i) {
+    (*func)(array[i]);
+  }
+}
+
+template <typename T>
+void doubleValue(T& val) {
+  val = val * 2;
+}
+
+#endif // ITER_HPP
---
+++ ex01/srcs/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "iter.hpp"
+
+void printHeader(std::string header) {
+  std::cout << "\n---" << header << "---\n" << std::endl;
+}
+
+int main() {
+
+  std::size_t array_size = 10;
+  {
+    printHeader("Test for char");
+    char array[10];
+    for (std::size_t i = 0; i < array_size; ++i) {
+      array[i] = '!' + static_cast<char>(i);
+    }
+    std::cout << "Original array: ";
+    ::iter(array, array_size, ::printValue);
+    std::cout << std::endl;
+    ::iter(array, array_size, doubleValue);
+    std::cout << "After doubled: ";
+    ::iter(array, array_size, ::printValue);
+    std::cout << std::endl;
+  }
+  {
+    printHeader("Test for int");
+    int array[10];
+    for (std::size_t i = 0; i < array_size; ++i) {
+      array[i] = i + 1;
+    }
+    std::cout << "Original array: ";
+    ::iter(array, array_size, ::printValue);
+    std::cout << std::endl;
+    ::iter(array, array_size, doubleValue);
+    std::cout << "After doubled: ";
+    ::iter(array, array_size, ::printValue);
+    std::cout << std::endl;
+  }
+  {
+    printHeader("Test for float");
+    float array[10];
+    for (std::size_t i = 0; i < array_size; ++i) {
+      array[i] = static_cast<float>(i) + 0.2f;
+    }
+    std::cout << "Original array: ";
+    ::iter(array, array_size, ::printValue);
+    std::cout << std::endl;
+    ::iter(array, array_size, doubleValue);
+    std::cout << "After doubled: ";
+    ::iter(array, array_size, ::printValue);
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
