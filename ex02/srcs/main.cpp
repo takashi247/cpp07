eb115ea6a0e4dd781c0df096a19b603576a45a33
takashi247 <takashi247@gmail.com>
2022-02-09 23:05:17 +0900

    add test cases for ex02

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -14,6 +14,12 @@ int main() {
   std::cout << "size of arr_empty_cpy: " << arr_empty_cpy.size() << std::endl;
   std::cout << "size of arr_empty_assign: " << arr_empty_assign.size() << std::endl;
 
+  try {
+    arr_empty[0] = 0;
+  } catch (const std::exception&) {
+    std::cout << "invalid index" << std::endl;
+  }
+
   // test normal case
   Array<int> arr(10);
   Array<int> arr_cpy(arr);
@@ -37,6 +43,30 @@ int main() {
     arr[arr.size()] = 100;
   } catch (const std::exception&) {
     std::cout << "invalid index" << std::endl;
-    return (EXIT_FAILURE);
+  }
+
+  // test array of string class
+  Array<std::string> arr_str(5);
+  Array<std::string> arr_str_cpy(arr_str);
+  Array<std::string> arr_str_assign;
+  arr_str_assign = arr_str;
+
+  std::cout << "size of arr_str: " << arr_str.size() << std::endl;
+  std::cout << "size of arr_str_cpy: " << arr_str_cpy.size() << std::endl;
+  std::cout << "size of arr_str_assign: " << arr_str_assign.size() << std::endl;
+
+  for (unsigned int i = 0; i < arr_str.size(); ++i) {
+    arr_str[i] = static_cast<char>(i + 42);
+    arr_str_cpy[i] = static_cast<char>(i + 52);
+    arr_str_assign[i] = static_cast<char>(i + 62);
+    std::cout << "arr["  << i << "] = " << arr_str[i] << "  "
+              << "arr_cpy[" << i << "] = " << arr_str_cpy[i] << "   "
+              << "arr_assign[" << i << "] = " << arr_str_assign[i] << std::endl;
+  }
+
+  try {
+    arr_str[arr_str.size()] = "hoge";
+  } catch (const std::exception&) {
+    std::cout << "invalid index" << std::endl;
   }
 }
