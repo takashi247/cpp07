713460846fdb2bf596150e93f07b7b9033f9caf5
takashi247 <takashi247@gmail.com>
2022-02-13 20:35:47 +0900

    add const version of iter

--- ex01/include/iter.hpp
+++ ex01/include/iter.hpp
@@ -5,12 +5,14 @@
 #include <iostream>
 
 template <typename T>
-void printValue(T& value) {
-  std::cout << value << " ";
+void iter(T* array, const std::size_t len, void(*func)(T&)) {
+  for (std::size_t i = 0; i < len; ++i) {
+    (*func)(array[i]);
+  }
 }
 
 template <typename T>
-void iter(T* array, std::size_t len, void(*func)(T&)) {
+void iter(const T* array, const std::size_t len, void(*func)(const T&)) {
   for (std::size_t i = 0; i < len; ++i) {
     (*func)(array[i]);
   }
@@ -19,6 +21,11 @@ void iter(T* array, std::size_t len, void(*func)(T&)) {
 template <typename T>
 void doubleValue(T& val) {
   val += val;
+}
+
+template <typename T>
+void printValue(const T& value) {
+  std::cout << value << " ";
 }
 
 #endif // ITER_HPP
